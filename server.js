f39062fddcbe1119b09d56925bf13626746188bb
danish <bestbarb43@gmail.com>
2021-07-21 10:45:41 -0700

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+const PORT = process.env.PORT || 3000;
+app.use(expressLayouts);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
