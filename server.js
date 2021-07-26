d5c29cf388b6b03fb2ed5fdc87bb2dea12cf5769
danish <bestbarb43@gmail.com>
2021-07-26 08:44:51 -0700

    done cart page

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 
 app.set("views", path.join(__dirname, "/resources/views"));
+app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.use(express.static("public"))
+
+
 app.get("/", (req, res) => {
   res.render("home");
 });
+app.get("/cart",(req,res)=>{
+  res.render("customers/cart")
+
+})
 const PORT = process.env.PORT || 3000;
-app.use(expressLayouts);
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
